72151b605afc11627929d7f690fed9aa22d24c04
Ryo5 <ffxllxxl@gmail.com>
2020-04-22 12:53:21 +0000

    fix4

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_123945) do
+ActiveRecord::Schema.define(version: 2020_04_21_155028) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
